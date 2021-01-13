a16070610a990ecba2a34e64165582fd22fcd59b
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-01-13 14:04:13 +0100

    todo list

--- ModulesTalk/ModulesTalk.cpp
+++ ModulesTalk/ModulesTalk.cpp
@@ -1,5 +1,12 @@
 import numbers;
 
+//TODO
+//1. Second module where a not exported function shares a name with a (exported) function from the first module
+//2. module parts / fragments
+//3. Header units
+//4. templates / concepts
+//5. export structs / classes
+
 int main()
 {
 	
