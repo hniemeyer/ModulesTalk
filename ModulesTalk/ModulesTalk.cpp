51df89971eb00b41ba7f67692908a038ddc95701
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-01-13 13:38:15 +0100

    print function.

--- ModulesTalk/ModulesTalk.cpp
+++ ModulesTalk/ModulesTalk.cpp
@@ -3,8 +3,8 @@ import numbers;
 
 int main()
 {
-	const auto res = get_magic_number();
+	
 	//const auto res2 = helper(5); //Will not compile
-    std::cout << res;
+	print_magic_number();
 }
 
--- ModulesTalk/numbers.cpp
+++ ModulesTalk/numbers.cpp
@@ -1,3 +1,6 @@
+module;
+#include <iostream>
+
 module numbers;
 
 int get_magic_number()
@@ -8,4 +11,10 @@ int get_magic_number()
 int helper(int x)
 {
 	return x + 42;
+}
+
+void print_magic_number()
+{
+	const auto res = get_magic_number();
+	std::cout << res;
 }
