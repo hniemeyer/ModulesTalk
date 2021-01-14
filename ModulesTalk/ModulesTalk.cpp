759e2615915afc8aaf73e340deca1fa82d80bda9
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-01-14 15:11:16 +0100

    second module and macro and name isolation

--- ModulesTalk/ModulesTalk.cpp
+++ ModulesTalk/ModulesTalk.cpp
@@ -1,17 +1,17 @@
 import numbers;
+import more_numbers;
 
 //TODO
-//1. Second module where a not exported function shares a name with a (exported) function from the first module
 //2. module parts / fragments
 //3. Header units
 //4. templates / concepts
 //5. export structs / classes
-//6. Demonstrate the isolation of macros
 
 int main()
 {
 	
 	//const auto res2 = helper(5); //Will not compile since not exported.
 	print_magic_number();
+	const auto res3 = more_magic();
 }
 
---
+++ ModulesTalk/more_numbers.ixx
@@ -0,0 +1,17 @@
+export module more_numbers;
+
+#define MAGIC 21
+
+int helper(int x)
+{
+	return x + MAGIC;
+}
+
+int get_magic_number()
+{
+	return helper(5);
+}
+
+export int more_magic() {
+	return 1 + get_magic_number();
+}
