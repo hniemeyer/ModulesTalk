9338d18b98c57ea2858b5718a8f2c96df46067f0
Hendrik Niemeyer <hendrik.niemeyer@googlemail.com>
2021-01-17 10:35:37 +0100

    Removed all done steps from todo comment

--- ModulesTalk/ModulesTalk.cpp
+++ ModulesTalk/ModulesTalk.cpp
@@ -2,10 +2,6 @@ import numbers;
 import more_numbers;
 import printer;
 
-// TODO
-// 2. module parts / fragments
-// 5. export structs / classes
-
 int main() {
 
   // const auto res2 = helper(5); //Will not compile since not exported.
